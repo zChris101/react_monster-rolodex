d29db2afd63311b0284eb7839e6aaaac47941363
zCris101 <chrstnmusico@gmail.com>
2023-07-12 13:16:58 +0800

    stable build

--- src/App.js
+++ src/App.js
@@ -7,27 +7,45 @@ class App extends Component {
 
   constructor() {
     super();
-
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
 
     }
+
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users)=>this.setState(()=>{
-        return { monsters: users}
+      .then((users) => this.setState(() => {
+        return { monsters: users }
       }));
   }
 
   render() {
 
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
+
     return (
       <div className="App">
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+
+            this.setState(() => {
+              return { searchField }; 
+            })
+          }}
+        />
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>
