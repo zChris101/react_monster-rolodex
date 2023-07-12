1de4f7e47e77b5353a1aa1778c1e15d1f6c191cf
zCris101 <chrstnmusico@gmail.com>
2023-07-12 13:44:20 +0800

    optimized .this  and .this.state

--- src/App.js
+++ src/App.js
@@ -28,10 +28,14 @@ class App extends Component {
 
   render() {
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters , searchField } =this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
+    
 
     return (
       <div className="App">
@@ -39,7 +43,7 @@ class App extends Component {
           className='search-box'
           type='search'
           placeholder='search monsters'
-          onChange={this.onSearchChange}
+          onChange={onSearchChange}
         />
         {
           filteredMonsters.map((monster) => {
